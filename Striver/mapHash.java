239ba935369bd5f49e9c03df494fd7168358f714
Abu Jubair Md Sahil <rcjubair786@gmail.com>
2025-07-30 13:35:14 +0530

    Java

---
+++ Striver/mapHash.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class mapHash {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the map");
+        int n = sc.nextInt();
+        System.out.println("Enter the elements in the map");
+        int arr[] = new int[n];
+        for(int i=0; i< n; i++){
+            arr[i] = sc.nextInt();
+        }
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for(int i=0; i< n; i++){
+            map[arr[i]]
+
+
+
+        int q;
+        q = sc.nextInt();
+        while (q-- != 0) {
+            int number;
+            number = sc.nextInt();
+            
+            
+        }
+        }
+
+}
