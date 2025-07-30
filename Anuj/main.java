d1c72f5f9cc2b4f6c56d06c1a8d860bcb255170d
Abu Jubair Md Sahil <rcjubair786@gmail.com>
2025-07-30 23:20:57 +0530

    oops

---
+++ Anuj/main.java
@@ -0,0 +1,32 @@
+public class main {
+    static class Person {
+        String name;
+        int age;
+
+        void walk() {
+            System.out.println(name + " is walking.");
+        }
+
+        void eat() {
+            System.out.println(name + " is eating.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.name = "Sahil";
+        p1.age = 25;
+        System.out.println(p1.name + " is " + p1.age + " years old.");
+        p1.walk();
+        p1.eat();
+
+        Person p2 = new Person();
+        p2.name = "John";
+        p2.age = 30;
+        System.out.println(p2.name + " is " + p2.age + " years old.");
+        p2.walk();
+        p2.eat();
+
+    }
+
+}
